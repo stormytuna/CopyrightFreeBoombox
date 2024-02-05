0f148e7f3c55eacaf39a74cb168e02f32fafbea4
stormytuna <stormytuna@outlook.com>
2024-02-05 16:40:19 +0000

    Initial commit

---
+++ CopyrightFreeBoombox/Patches/BoomboxItemPatch.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CopyrightFreeBoombox.Patches
+{
+	[HarmonyPatch(typeof(BoomboxItem))]
+	public class BoomboxItemPatch
+	{
+		private static bool IsNotCopyrighted(AudioClip audioClip) {
+			switch (audioClip.name) {
+				case "BoomboxMusic1": return true;
+				case "BoomboxMusic2": return true;
+				case "BoomboxMusic3": return true;
+				case "BoomboxMusic4": return true;
+				default: return false;
+			}
+		}
+
+		[HarmonyPrefix]
+		[HarmonyPatch(nameof(BoomboxItem.StartMusic))]
+		public static void RemoveCopyrightedMusic(ref AudioClip[] ___musicAudios) {
+			if (___musicAudios is null) {
+				CopyrightFreeBoomboxBase.Log.LogError("Failed to patch out copyrighted music!!");
+				return;
+			}
+
+			CopyrightFreeBoomboxBase.Log.LogInfo("BEFORE");
+			foreach (AudioClip music in ___musicAudios) {
+				CopyrightFreeBoomboxBase.Log.LogInfo(music.name);
+			}
+
+			___musicAudios = ___musicAudios.Where(IsNotCopyrighted).ToArray();
+
+			CopyrightFreeBoomboxBase.Log.LogInfo("AFTER");
+			foreach (AudioClip music in ___musicAudios) {
+				CopyrightFreeBoomboxBase.Log.LogInfo(music.name);
+			}
+		}
+	}
+}
---
+++ CopyrightFreeBoombox/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace CopyrightFreeBoombox
+{
+	[BepInPlugin(ModGUID, ModName, ModVersion)]
+	public class CopyrightFreeBoomboxBase : BaseUnityPlugin
+	{
+		public const string ModGUID = "stormytuna.CopyrightFreeBoombox";
+		public const string ModName = "CopyrightFreeBoombox";
+		public const string ModVersion = "1.0.0";
+
+		public static ManualLogSource Log = BepInEx.Logging.Logger.CreateLogSource(ModGUID);
+		public static CopyrightFreeBoomboxBase Instance;
+
+		private readonly Harmony harmony = new Harmony(ModGUID);
+
+		private void Awake() {
+			if (Instance is null) {
+				Instance = this;
+			}
+
+			Log.LogInfo("Copyright Free Boombox has awoken!");
+
+			harmony.PatchAll();
+		}
+	}
+}
